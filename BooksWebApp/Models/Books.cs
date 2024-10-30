78c8f4379a5fedb365b8892757719b129b6fc832
LMHD <LMHD@DESKTOP-T5S096L>
2024-10-30 11:15:54 +0100

    Ajoutez des fichiers projet.

---
+++ BooksWebApp/Models/Books.cs
@@ -0,0 +1,15 @@
+﻿namespace BooksWebApp.Models
+{
+    public class Books
+    {
+        public int Id { get; set; } 
+
+        public string Title { get; set; } = null!;
+        public string description { get; set; } = null!;
+
+        public Books()
+        {
+        }
+
+    }
+}
